629b545ca31d67a0927bdaa38443537ed0dd6716
Shital25x <shitalsinha18@gmail.com>
2024-10-05 20:07:32 +0530

    asyncstorage file

---
+++ compo/asyncstorage/Asyncstorage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Asyncstorage = () => {
+  return (
+    <View>
+      <Text>Asyncstorage</Text>
+    </View>
+  )
+}
+
+export default Asyncstorage
+
+const styles = StyleSheet.create({})
