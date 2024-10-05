0ff4a36e0590f29b18351127329f6b4162f4b237
Shital25x <shitalsinha18@gmail.com>
2024-10-05 20:11:30 +0530

    asyncstorage file made

--- App.js
+++ App.js
@@ -86,6 +86,7 @@ import Stack from './compo/navigation/Stack'
 import Simplevalidation from './compo/validatingform/Simplevalidation'
 import Validationwithconditionalrendering from './compo/validatingform/Validationwithconditionalrendering'
 import Crud from './compo/crud/Todo'
+
 const App = () => {
   return (
     <View style={styles.container}>
@@ -106,6 +107,7 @@ const App = () => {
 {/* <Stack/> */}
 {/* <Simplevalidation/> */}
 {/* <Validationwithconditionalrendering/> */}
+
     </View>
   )
 }
